1e557864fabe49b9a1351e4057d95eb3130af226
izak <izabkru88@gmail.com>
2021-05-18 15:28:51 +0100

    Styling Home section.

---
+++ src/components/About/About.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './About.module.css';
+
+export default function AboutPage() {
+  return <h1 className={styles.about}>About</h1>;
+}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,35 +2,43 @@ import React from 'react';
 
 import styles from './Home.module.css';
 import hero from '../../assets/images/hero.jpg';
+import LazyHero from 'react-lazy-hero';
 
 export default function HomePage() {
   return (
     <section className={styles.home} id='home'>
-      <div className={styles.image}>
-        <img src={hero} alt='Hero' />
-      </div>
+      <LazyHero
+        className={styles.image}
+        imageSrc={hero}
+        parallaxOffset={100}
+        opacity={0.4}
+        color='#000'
+        minHeight='100vh'
+        transitionDuration={800}
+        transitionTimingFunction='ease-in-out'
+      >
+        <div className={styles.heading}>
+          <div className={styles.heading1}>
+            <h1>welcome to</h1> <br />
+          </div>
+          <div className={styles.heading2}>
+            <h2>
+              <span>MT</span>Elevators
+            </h2>
 
-      <div className={styles.heading}>
-        <div className={styles.heading1}>
-          <h1>welcome to</h1> <br />
+            <h3>
+              Plan <br /> Installation <br /> Modenization <br /> Service <br />{' '}
+              Repair of Elevators
+            </h3>
+          </div>
+          <div className={styles.text}>
+            <p>We will take you to a higher level</p>
+            <div className={styles.btn}>
+              <a href='/'>More</a>
+            </div>
+          </div>
         </div>
-        <div className={styles.heading2}>
-          <h2>
-            <span>MT</span> Elevators Ltd
-          </h2>
-          <h3>
-            plan, installation, modenization, service and repair of elevators
-          </h3>
-        </div>
-      </div>
-
-      <div className={styles.text}>
-        <p>Take Yourself To A Higher Level</p>
-
-        <div className={styles.btn}>
-          <a href='/'>More</a>
-        </div>
-      </div>
+      </LazyHero>
     </section>
   );
 }
