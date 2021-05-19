aac2a9e2f64b4c0cf649956bec592c3f6a76fc2d
izak <izabkru88@gmail.com>
2021-05-19 12:23:50 +0100

    Made  a sticky Navbar that hides on scroll.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,39 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../Logo/Logo';
+import { debounce } from '../../utilities/helpers';
+import { useState, useEffect } from 'react';
 
 export default function Toolbar() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(() => {
+    // find current scroll position
+    const currentScrollPos = window.pageYOffset;
+
+    // set state based on location info
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
+    );
+
+    // set state to new scroll position
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
-    <header className={styles.toolbar}>
+    <header
+      className={styles.toolbar}
+      style={{ top: visible ? '0' : '-220px' }}
+    >
       <div className={styles.logo}>
         <Logo id='home' />
       </div>
