7a299544c88eafae1e9475218dd8f0e32342ba89
izak <izabkru88@gmail.com>
2021-05-19 11:33:02 +0100

    Added logo.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo1 from '../../assets/images/logo.png';
+import styles from './Logo.module.css';
+import { Link } from 'react-scroll';
+
+const logo = (props) => (
+  <div className={styles.logo}>
+    <Link smooth='easeInOutCubic' duration={2000} to={props.id}>
+      <img src={logo1} alt='Logo' />
+    </Link>
+  </div>
+);
+
+export default logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Logo from '../../Logo/Logo';
 
 export default function Toolbar() {
   return (
     <header className={styles.toolbar}>
+      <div className={styles.logo}>
+        <Logo id='home' />
+      </div>
       <nav className={styles.desktop__only}>
         <NavigationItems />
       </nav>
