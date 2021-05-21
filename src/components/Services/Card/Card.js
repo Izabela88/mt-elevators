519e92cd8bef20c9cef9ddd9736b012b067e36d0
izak <izabkru88@gmail.com>
2021-05-21 17:39:58 +0100

    Styling card component.

--- src/components/Services/Card/Card.js
+++ src/components/Services/Card/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './Card.module.css';
-import icon from '../../../assets/icons/icon1.svg';
 
 const Card = (props) => {
   return (
     <div className={styles.card}>
       <div className={styles.card__icon}>
-        <img src={icon} alt={props.alt} />
+        <img src={props.src} alt={props.alt} />
       </div>
+
       <div className={styles.card__text}>
-        <p>Services</p>
+        <p>{props.text}</p>
       </div>
     </div>
   );
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from './Services.module.css';
 import Card from './Card/Card';
+import icon1 from '../../assets/icons/icon1.svg';
+import icon2 from '../../assets/icons/icon2.svg';
 
 export default function Services() {
   return (
     <section id='services' className={styles.services}>
       <div className={styles.container}>
         <div className={styles.cards}>
-          <Card />
-          <Card />
+          <Card src={icon1} text='Plan' />
+          <Card src={icon2} text='Service' />
           <Card />
         </div>
       </div>
