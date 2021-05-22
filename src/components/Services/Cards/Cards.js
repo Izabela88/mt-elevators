659010769fda4ebe45e52b0b0271f6256bc46b84
izak <izabkru88@gmail.com>
2021-05-22 17:24:12 +0100

    Changed files structure in Service section, basic styling card component.

--- src/components/Services/Cards/Cards.js
+++ src/components/Services/Cards/Cards.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './Cards.module.css';
-import Card from '../Card/Card';
+import './cards.css';
+import React, { useEffect, useCallback, useState } from 'react';
+
 import icon1 from '../../../assets/icons/icon7.svg';
 import icon2 from '../../../assets/icons/icon12.svg';
 import icon3 from '../../../assets/icons/icon5.svg';
@@ -11,18 +11,114 @@ import icon7 from '../../../assets/icons/icon9.svg';
 import icon8 from '../../../assets/icons/icon2.svg';
 import icon9 from '../../../assets/icons/icon13.svg';
 
-const Cards = (props) => {
+const cardItems = [
+  {
+    id: 1,
+    title: 'Plan',
+    image: icon1,
+  },
+  {
+    id: 2,
+    title: 'Installation',
+    image: icon2,
+  },
+  {
+    id: 3,
+    title: 'Modernization',
+    image: icon3,
+  },
+  {
+    id: 4,
+    title: 'Service',
+    image: icon4,
+  },
+  {
+    id: 5,
+    title: 'Fault Finding',
+    image: icon5,
+  },
+  {
+    id: 6,
+    title: 'Electric services',
+    image: icon6,
+  },
+  {
+    id: 7,
+    title: 'Repairs',
+    image: icon7,
+  },
+  {
+    id: 8,
+    title: 'Realization With Certificate',
+    image: icon8,
+  },
+  {
+    id: 9,
+    title: 'Passenger and Goods Lifts',
+    image: icon9,
+  },
+];
+
+function determineClasses(indexes, cardIndex) {
+  if (indexes.currentIndex === cardIndex) {
+    return 'active';
+  } else if (indexes.nextIndex === cardIndex) {
+    return 'next';
+  } else if (indexes.previousIndex === cardIndex) {
+    return 'prev';
+  }
+  return 'inactive';
+}
+
+const Cards = () => {
+  const [indexes, setIndexes] = useState({
+    previousIndex: 0,
+    currentIndex: 0,
+    nextIndex: 1,
+  });
+
+  const handleCardTransition = useCallback(() => {
+    // If we've reached the end, start again from the first card,
+    // but carry previous value over
+    if (indexes.currentIndex >= cardItems.length - 1) {
+      setIndexes({
+        previousIndex: cardItems.length - 1,
+        currentIndex: 0,
+        nextIndex: 1,
+      });
+    } else {
+      setIndexes((prevState) => ({
+        previousIndex: prevState.currentIndex,
+        currentIndex: prevState.currentIndex + 1,
+        nextIndex:
+          prevState.currentIndex + 2 === cardItems.length
+            ? 0
+            : prevState.currentIndex + 2,
+      }));
+    }
+  }, [indexes.currentIndex]);
+
+  useEffect(() => {
+    const transitionInterval = setInterval(() => {
+      handleCardTransition();
+    }, 2500);
+
+    return () => clearInterval(transitionInterval);
+  }, [handleCardTransition, indexes]);
+
   return (
-    <div className={styles.cards}>
-      <Card src={icon1} text='Plan' />
-      <Card src={icon2} text='Installation' />
-      <Card src={icon3} text='Modernization' />
-      <Card src={icon4} text='Service' />
-      <Card src={icon5} text='Fault Finding' />
-      <Card src={icon6} text='Electric services' />
-      <Card src={icon7} text='Repairs' />
-      <Card src={icon8} text='Realization With Certificate' />
-      <Card src={icon9} text='Passenger and Goods Lifts' />
+    <div className='card-carousel'>
+      {cardItems.map((card, index) => (
+        <div
+          key={card.id}
+          className={`card ${determineClasses(indexes, index)}`}
+        >
+          <h2>{card.title}</h2>
+          <div className='card__icon'>
+            <img src={card.image} alt='Card Slide' />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -6,7 +6,10 @@ export default function Services() {
   return (
     <section id='services' className={styles.services}>
       <div className={styles.container}>
-        <Cards />
+        <h1>OUR SERVICES</h1>
+        <div className={styles.cards__box}>
+          <Cards />
+        </div>
       </div>
     </section>
   );
