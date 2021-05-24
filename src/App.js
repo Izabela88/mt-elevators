64f261fc34f71d9dec5fa2a3fd5758bea94a79c1
izak <izabkru88@gmail.com>
2021-05-24 19:22:53 +0100

    Basic file structure.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Toolbar from './components/Navigation/Toolbar/Toolbar';
 import Services from './components/Services/Services';
+import Choice from './components/Choice/Choice';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Services />
+      <Choice />
     </Fragment>
   );
 }
---
+++ src/components/Choice/Choice.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Choice.module.css';
+
+export default function Choice() {
+  return (
+    <section className={styles.choice}>
+      <h1>Why Choose Us</h1>
+    </section>
+  );
+}
