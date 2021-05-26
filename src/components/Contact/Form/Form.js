8d1d6bae3a37dd5ecc6bcdd224f46296df0acb08
izak <izabkru88@gmail.com>
2021-05-26 20:17:11 +0100

    Contact form validation.

--- src/components/Contact/Form/Form.js
+++ src/components/Contact/Form/Form.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+
 import styles from './Form.module.css';
 
 export default function ContactForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isDirty },
   } = useForm();
   const onSubmit = (data) => {
     alert(JSON.stringify(data));
@@ -15,16 +16,19 @@ export default function ContactForm() {
   return (
     <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <input
+        className={styles.input}
         placeholder='First Name'
         {...register('firstName', {
           validate: (value) => value.length > 2 && value.length < 20,
           pattern: /^[A-Za-z]+$/i,
           required: true,
+          isSubmitting: true,
         })}
       />
       {errors.firstName && <p>Incorrect name!</p>}
 
       <input
+        className={styles.input}
         placeholder='Last Name'
         {...register('lastName', {
           validate: (value) => value.length > 2 && value.length < 40,
@@ -35,12 +39,14 @@ export default function ContactForm() {
       {errors.lastName && <p>Incorrect last name!</p>}
 
       <input
+        className={styles.input}
         placeholder='Email'
         type='email'
         {...register('email', {
           required: true,
         })}
       />
+      {errors.message && <p>Required!</p>}
 
       <textarea
         placeholder='Your Message'
@@ -52,7 +58,7 @@ export default function ContactForm() {
       />
       {errors.message && <p>Required!</p>}
 
-      <input type='submit' />
+      <input disabled={!isDirty} className={styles.btn} type='submit' />
     </form>
   );
 }
